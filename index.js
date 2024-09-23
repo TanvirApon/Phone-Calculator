ac13110dd55a4fa25f1e50e811c660324f6722d4
TanvirApon <tanvirhossainapon@gmail.com>
2024-09-23 16:00:00 +0600

    Code updated

--- index.js
+++ index.js
@@ -1,82 +1,141 @@
-console.log("Js file has been added");
 
-let result = 0;
+console.log("Js file has been added");
+var num = "", result = 0 , operator = "+";
 
 function getValue(inputValue){
-    document.getElementById("display").value +=inputValue;
-    let str = document.getElementById("display").value;
-    check(str);
-}
-
-function check(str){
-    let operators=['+','-','/','*'];
-    let index; 
-    let operator;
-    for(let i=0;i<str.length;i++){
-        if(operators.includes(str[i]) && operators.includes(str[i+1])|| operators.includes(str[0])){
-            alert("Error");
-            document.getElementById("display").value = "";
-        }
-        else if(operators.includes(str[i]))
-        {
-            index=i;
-            operator=str[i];
-        }
-        else{
-            //console.log("no expression found");
+    var operators = ["+", "-", "*", "/"];
+    for(let i=0;i<inputValue.length;i++){
+        if (operators.includes(inputValue[i])) {
+            calculation(); 
+            operator = inputValue[i]; 
+            console.log("get Value function operator: " + operator);
+            num = ""; 
+        } 
+        else {
+            num += inputValue[i];
         }
     }
-    let firstNum = parseFloat(str.slice(0,index));
-    let secNum = parseFloat(str.slice(index+1));
-
-    console.log("Checked function firstNum "+ firstNum);
-
-    console.log("First Number from check function : "+ firstNum );
-    console.log("Second Number from check function : " + secNum);
-    console.log(operator);
-
-  
-
-    calculate(firstNum,secNum,operator);
+    console.log("Result: " + result);
+    document.getElementById("display").value += inputValue;   
 }
-
-function calculate(firstNum,secNum,operator){
-    firstNum === result;
-    console.log("Function calculate first line Result: " + firstNum);
-
-    if(operator==='+')
-    {
-      result === firstNum;
-      console.log("Result from calculator function: "+ result );
-      console.log("First Number from calculator function: "+ firstNum );
-      console.log("Second Number from calculator function: "+ secNum );
-      result += secNum;   
+function calculation(){
+    if (num !== "") {
+        if (operator === "+") {
+            result += parseFloat(num);
+        } else if (operator === "-") {
+            result -= parseFloat(num);
+        } else if (operator === "*") {
+            result *= parseFloat(num);
+        } else if (operator === "/") {
+            result /= parseFloat(num);
+        }
     }
-    if(operator==='-')
-    {
-        console.log("First Number from calculator function: "+ firstNum );
-        result -= secNum;
-    }
-    if(operator==='*')
-    {
-        console.log("First Number from calculator function: "+ firstNum );
-        result *= secNum;
-    }
-    if(operator==='/')
-    {
-        console.log("First Number from calculator function: "+ firstNum );
-        result /= secNum;
-    }
-    console.log("Result from calculator function : "+ result);  
-   
+    console.log("Result: " + result);
+    document.getElementById("display").value = result;  
 }
-
+function calculateFinal() {
+    calculation();
+    document.getElementById("display").value = result; 
+    num ="";  
+}
 function clearAll(){
     document.getElementById("display").value = ""; 
+    num = "";
+    result = 0;
+    operator = "";
     console.clear();  
 }
 
-function float(){
-  
 
-}
+
+
+
+
+/*
+
+Old Version of code which has some error 
+of updating the values of firstNum
+*/
+// console.log("Js file has been added");
+
+// let result = 0;
+
+// function getValue(inputValue){
+//     document.getElementById("display").value +=inputValue;
+//     let str = document.getElementById("display").value;
+//     check(str);
+// }
+// function check(str){
+//     let operators=['+','-','/','*'];
+//     let index; 
+//     let operator;
+//     for(let i=0;i<str.length;i++){
+//         if(operators.includes(str[i]) && operators.includes(str[i+1])|| operators.includes(str[0])){
+//             alert("Error");
+//             document.getElementById("display").value = "";
+//         }
+//         else if(operators.includes(str[i]))
+//         {
+//             index=i;
+//             operator=str[i];
+//         }
+//         else{
+//             //console.log("no expression found");
+//         }
+//     }
+//     let firstNum = parseFloat(str.slice(0,index));
+//     let secNum = parseFloat(str.slice(index+1));
+
+//     console.log("Checked function firstNum "+ firstNum);
+//     console.log("First Number from check function : "+ firstNum );
+//     console.log("Second Number from check function : " + secNum);
+//     console.log(operator);
+
+//     calculate(firstNum,secNum,operator);
+// }
+// function calculate(firstNum,secNum,operator){
+    
+   
+
+//     console.log("Function calculate first line Result: " + firstNum);
+
+//     if(operator === '+')
+//     {
+ 
+//       console.log("Result from calculator function: "+ result );
+//       console.log("First Number from calculator function: "+ firstNum );
+//       console.log("Second Number from calculator function: "+ secNum );
+      
+//       result = firstNum + secNum; 
+//       firstNum = result;
+       
+//     }
+//     if(operator === '-')
+//     {
+//         console.log("First Number from calculator function: "+ firstNum );
+//         result = firstNum - secNum;
+//     }
+//     if(operator === '*')
+//     {
+//         console.log("First Number from calculator function: "+ firstNum );
+//         result = firstNum * secNum;
+//     }
+//     if(operator === '/')
+//     {
+//         console.log("First Number from calculator function: "+ firstNum );
+//         result = firstNum / secNum;
+//     }
+
+   
+    
+
+    
+//     console.log("Result from calculator function : "+ result); 
+// }
+// function clearAll(){
+//     document.getElementById("display").value = ""; 
+//     console.clear();  
+// }
+// function float(){
+  
+// }
